8e0be01ecf10681057e468334d2d0034f24e03cd
冯尚实 <fengshangshi@fengshangshideMacBook-Pro.local>
2017-04-26 11:56:51 +0800

    增加json2file json2toml方法

--- README.md
+++ README.md
@@ -1,9 +1,11 @@
-#sugar-toml
+# sugar-toml
 
 sugar2.0框架的配置文件组件，TOML不了解的可以自行百度。
 
-##API
-###load()
+## API
+
+
+### file2json(file)
 
 会做两个事情：
 
@@ -12,16 +14,82 @@ sugar2.0框架的配置文件组件，TOML不了解的可以自行百度。
 
 ```
 const toml = require('sugar-toml');
-const config = toml.load('./index.toml');
+const config = toml.file2json('index.toml');
 console.log(config);
+
 ```
 
-###parse()
+### file2json(file, callback)
+file2json的异步版本，callback使用如下：
+
+```
+toml.file2json('index.toml', (err, source) => {
+	if (err) {
+		// 做点啥
+	}	
+	console.log(source);
+});
+
+```
+callback参数说明：
+
+* err - 读取文件的错误信息，正常时为null
+* source - 读取解析之后的JSON对象，异常时为null
+
+
+### toml2json(source)
 这个API假定你已经有一个toml的格式的字符串，用这个api来解析：
 
 ```
 const fs = require('fs');
 const source = fs.readFileSync('./index.toml', {encoding: 'utf8'});
-const config = toml.parse(source);
+const config = toml.toml2json(source);
 console.log(config);
 ```
+
+### json2toml(source)
+会把json转化成toml字符创，如下：
+
+```
+let json = {
+	date: new Date(), 
+	arr: [1, 2, 3], 
+	obj: {
+		num: 123,
+		a: {
+			a_b: 1, 
+			a_c: 2,
+		}
+	}
+};
+var tomlAsString = toml.json2toml(json); 
+//'arr = [1,2,3]\ndate = 2017-04-25T14:04:24Z\n[obj]\nnum = 123\n[obj.a]\na_b = 1\na_c = 2\n'
+
+console.log(tomlAsString);
+/*
+arr = [1,2,3]
+date = 2017-04-25T14:04:24Z
+[obj]
+num = 123
+[obj.a]
+a_b = 1
+a_c = 2
+*/
+```
+
+### json2file(source, file)
+
+会做两个事情：
+
+1. 将json转化成toml字符创
+2. 写入到指定的file文件中
+
+```
+var isWrited = toml.json2file(json, './config.toml'); 
+console.log(isWrited); // true || error
+
+```
+
+### json2file(source, file, callback)
+
+json2file的异步版本，callback返回err状态，无异常为null
--- index.js
+++ index.js
@@ -4,21 +4,33 @@
  */
 'use strict'
 
-const semver = require('semver');
+const strftime = require('strftime');
+const type = require('sugar-type');
 const toml = require('toml');
 const fs = require('fs');
 
-exports.load = function(file) {
-    let options = semver.lt(process.versions.node, '0.10.0') ? 'utf8' : {
-        encoding: 'utf8'
-    };
+// 读写文件的选项
+const options = {
+    encoding: 'utf8'
+};
 
-    // 同步读取
+// 读取toml文件并转换成json
+// callback传入为异步版本
+exports.file2json = function(file, callback) {
+    // 异步版本
+    if (type.isFunction(callback)) {
+        return fs.readFile(file, options, (err, source) => {
+            callback(err, source);
+        });
+    }
+
+    // 同步版本
     let source = fs.readFileSync(file, options);
-    return exports.parse(source);
+    return exports.toml2json(source);
 }
 
-exports.parse = function(source) {
+// 将toml格式字符串转成json
+exports.toml2json = function(source) {
     try {
         return toml.parse(source);
     } catch (e) {
@@ -30,4 +42,66 @@ exports.parse = function(source) {
         }
         throw e;
     }
+}
+
+// 把json对象转换成toml格式字符串
+exports.json2toml = function(source) {
+    var tomlAsString = '';
+
+    // 漫游
+    function roam(hash, prefix) {
+        let nestedPairs = [];
+        let simplePairs = [];
+
+        prefix = prefix || '';
+
+        Object.keys(hash).sort().forEach((key) => {
+            let value = hash[key];
+            (type.isObject(value) ? nestedPairs : simplePairs).push([key, value]);
+        });
+
+        if (!(type.isEmpty(prefix) || type.isEmpty(simplePairs))) {
+            tomlAsString += '[' + prefix + ']\n';
+        }
+
+        simplePairs.forEach((array) => {
+            var key = array[0];
+            var value = array[1];
+
+            tomlAsString += key + ' = ' + format(value) + '\n';
+        });
+
+        nestedPairs.forEach((array) => {
+            var key = array[0];
+            var value = array[1];
+
+            roam(value, type.isEmpty(prefix) ? key.toString() : [prefix, key].join('.'));
+        });
+
+
+    }
+
+    roam(source);
+
+    return tomlAsString;
+};
+
+// 把json对象转换成toml格式字符串并写入到文件中
+// callback传入为异步版本
+exports.json2file = function(source, file, callback) {
+    let tomlAsString = exports.json2toml(source);
+
+    // 异步版本
+    if (type.isFunction(callback)) {
+        return fs.writeFile(file, tomlAsString, options, (err) => {
+            callback(err);
+        });
+    }
+
+    // 同步版本
+    fs.writeFileSync(file, tomlAsString, options);
+};
+
+function format(source) {
+    return type.isDate(source) ? strftime('%FT%TZ', source) : JSON.stringify(source);
 }
